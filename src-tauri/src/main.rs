9725cc7300c5082933c41f657adf69224f0aadcc
HushAsy <hushasy@gmail.com>
2026-03-30 14:16:13 +0800

    fix: support macOS DMG bundling with custom layout

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,5 +1,6 @@
-#![windows_subsystem = "windows"]
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 fn main() {
-    neeko::run()
+    neeko_lib::run()
 }
